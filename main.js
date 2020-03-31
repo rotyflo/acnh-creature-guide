645aaddfe1832fc7bec89f8a01226a5aac71a8cf
RT <rt@pop-os.localdomain>
2020-03-31 15:39:09 -0700

    Sort alphabetically, show '0-23' as 'all day' and add a few March fish

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let creatureNames = Object.keys(CREATURES);
 let today = new Date();
 let currentMonth = today.getMonth() + 1;
 let currentHour = today.getHours();
-let highPriorityCreatures = listHighPriorityCreatures(CREATURES, creatureNames);
-let availableCreatures = listAvailableCreatures(CREATURES, creatureNames);
+let highPriorityCreatures = listHighPriorityCreatures(CREATURES, creatureNames).sort();
+let availableCreatures = listAvailableCreatures(CREATURES, creatureNames).sort();
 
 printHighPriorityCreatures(CREATURES, highPriorityCreatures, 'high-priority-creatures');
 
@@ -98,10 +98,17 @@ function printHighPriorityCreatures(database, creatures, elementId) {
 
   for (let i = 0; i < creatures.length; i++) {
     let creature = creatures[i];
-    let firstHour = formatTime(database[creature]['time'][0]);
-    let lastHour = formatTime(database[creature]['time'][1]);
+    let firstHour = database[creature]['time'][0];
+    let lastHour = database[creature]['time'][1];
+
     output += `<tr><td>${formatName(creature)}</td>`;
-    output += `<td>${firstHour} - ${lastHour}</td></tr>`;
+
+    if (firstHour === 0 && lastHour === 23) {
+      output += '<td>all day</td>';
+    }
+    else {
+      output += `<td>${formatTime(firstHour)} - ${formatTime(lastHour)}</td></tr>`;
+    }
   }
 
   element.innerHTML += output + '</table>';
@@ -148,308 +155,338 @@ function generateCreatures() {
 
     // BUG LIST
 
-   'agrias butterfly': {
-     'price': 3000,
-     'location': 'flying around',
-     'time': [8, 17],
-     'months': [4, 5, 6, 7, 8, 9]
-   },
-   'ant': {
-     'price': 80,
-     'location': 'on rotten food',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'atlas moth': {
-     'price': 3000,
-     'location': 'on trees',
-     'time': [19, 4],
-     'months': [4, 5, 6, 7, 8, 9]
-   },
-   'bagworm': {
-     'price': 600,
-     'location': 'by shaking trees',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'banded dragonfly': {
-     'price': 4500,
-     'location': 'flying around',
-     'time': [8, 17],
-     'months': [5, 6, 7, 8, 9, 10]
-   },
-   'bell cricket': {
-     'price': 430,
-     'location': 'on the ground',
-     'time': [17, 8],
-     'months': [9, 10]
-   },
-   'blue weevil beetle': {
-     'price': 800,
-     'location': 'on coconut trees',
-     'time': [0, 23],
-     'months': [7, 8]
-   },
-   'brown cicada': {
-     'price': 250,
-     'location': 'on trees',
-     'time': [8, 17],
-     'months': [7, 8]
-   },
-   'centipede': {
-     'price': 300,
-     'location': 'by hitting rocks',
-     'time': [16, 23],
-     'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
-   },
-   'cicada shell': {
-     'price': 10,
-     'location': 'on trees',
-     'time': [0, 23],
-     'months': [7, 8]
-   },
-   // 'citrus long-horned beetle': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   'common bluebottle': {
-     'price': 300,
-     'location': 'flying around',
-     'time': [4, 19],
-     'months': [4, 5, 6, 7, 8]
-   },
-   'common butterfly': {
-     'price': 160,
-     'location': 'flying around',
-     'time': [4, 19],
-     'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
-   },
-   // 'cricket': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'cyclommatus stag': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'damselfly': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'darner dragonfly': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'diving beetle': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'drone beetle': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'dung beetle': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   // 'earth-boring dung beetle': {
-   //   'price': ,
-   //   'location': '',
-   //   'time': [],
-   //   'months': []
-   // },
-   'emperor butterfly': {
-     'price': 4000,
-     'location': 'flying around',
-     'time': [17, 8],
-     'months': [1, 2, 3, 6, 7, 8, 9, 12]
-   },
-   'fly': {
-     'price': 60,
-     'location': 'on trash items',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'great purple emperor': {
-     'price': 3000,
-     'location': 'flying around',
-     'time': [4, 19],
-     'months': [5, 6, 7, 8]
-   },
-   'hermit crab': {
-     'price': 1000,
-     'location': 'disguised as shells on the beach',
-     'time': [19, 8],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'honeybee': {
-     'price': 200,
-     'location': 'flying around',
-     'time': [8, 17],
-     'months': [3, 4, 5, 6, 7]
-   },
-   'ladybug': {
-     'price': 200,
-     'location': 'on flowers',
-     'time': [8, 17],
-     'months': [3, 4, 5, 6, 10]
-   },
-   'man-faced stink bug': {
-     'price': 1000,
-     'location': 'on flowers',
-     'time': [19, 8],
-     'months': [3, 4, 5, 6, 7, 8, 9, 10]
-   },
-   'mantis': {
-     'price': 430,
-     'location': 'on flowers',
-     'time': [8, 17],
-     'months': [3, 4, 5, 6, 7, 8, 9, 10, 11]
-   },
-   'mole cricket': {
-     'price': 500,
-     'location': 'underground',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 11, 12]
-   },
-   'moth': {
-     'price': 130,
-     'location': 'flying by light',
-     'time': [19, 4],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'orchid mantis': {
-     'price': 2400,
-     'location': 'on white flowers',
-     'time': [8, 17],
-     'months': [3, 4, 5, 6, 7, 8, 9, 10, 11]
-   },
-   'paper kite butterfly': {
-     'price': 1000,
-     'location': 'flying around',
-     'time': [8, 19],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'peacock butterfly': {
-     'price': 2500,
-     'location': 'flying by hybrid flowers',
-     'time': [4, 19],
-     'months': [3, 4, 5, 6]
-   },
-   'pill bug': {
-     'price': 250,
-     'location': 'by hitting rocks',
-     'time': [23, 16],
-     'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
-   },
-   'snail': {
-     'price': 250,
-     'location': 'on rocks when raining',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'spider': {
-     'price': 480,
-     'location': 'by shaking trees',
-     'time': [19, 8],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'stinkbug': {
-     'price': 120,
-     'location': 'on flowers',
-     'time': [0, 23],
-     'months': [3, 4, 5, 6, 7, 8, 9, 10]
-   },
-   'tarantula': {
-     'price': 8000,
-     'location': 'on the ground',
-     'time': [19, 4],
-     'months': [1, 2, 3, 4, 11, 12]
-   },
-   'tiger beetle': {
-     'price': 1500,
-     'location': 'on the ground',
-     'time': [0, 23],
-     'months': [2, 3, 4, 5, 6, 7, 8, 9, 10]
-   },
-   'tiger butterfly': {
-     'price': 240,
-     'location': 'flying around',
-     'time': [4, 19],
-     'months': [3, 4, 5, 6, 7, 8, 9]
-   },
-   'wasp': {
-     'price': 2500,
-     'location': 'by shaking trees',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'wharf roach': {
-     'price': 200,
-     'location': 'on beach rocks',
-     'time': [0, 23],
-     'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-   },
-   'yellow butterfly': {
-     'price': 160,
-     'location': 'flying around',
-     'time': [4, 19],
-     'months': [3, 4, 5, 6, 9, 10]
-   },
+    'agrias butterfly': {
+      'price': 3000,
+      'location': 'flying around',
+      'time': [8, 17],
+      'months': [4, 5, 6, 7, 8, 9]
+    },
+    'ant': {
+      'price': 80,
+      'location': 'on rotten food',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'atlas moth': {
+      'price': 3000,
+      'location': 'on trees',
+      'time': [19, 4],
+      'months': [4, 5, 6, 7, 8, 9]
+    },
+    'bagworm': {
+      'price': 600,
+      'location': 'by shaking trees',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'banded dragonfly': {
+      'price': 4500,
+      'location': 'flying around',
+      'time': [8, 17],
+      'months': [5, 6, 7, 8, 9, 10]
+    },
+    'bell cricket': {
+      'price': 430,
+      'location': 'on the ground',
+      'time': [17, 8],
+      'months': [9, 10]
+    },
+    'blue weevil beetle': {
+      'price': 800,
+      'location': 'on coconut trees',
+      'time': [0, 23],
+      'months': [7, 8]
+    },
+    'brown cicada': {
+      'price': 250,
+      'location': 'on trees',
+      'time': [8, 17],
+      'months': [7, 8]
+    },
+    'centipede': {
+      'price': 300,
+      'location': 'by hitting rocks',
+      'time': [16, 23],
+      'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
+    },
+    'cicada shell': {
+      'price': 10,
+      'location': 'on trees',
+      'time': [0, 23],
+      'months': [7, 8]
+    },
+    // 'citrus long-horned beetle': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    'common bluebottle': {
+      'price': 300,
+      'location': 'flying around',
+      'time': [4, 19],
+      'months': [4, 5, 6, 7, 8]
+    },
+    'common butterfly': {
+      'price': 160,
+      'location': 'flying around',
+      'time': [4, 19],
+      'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
+    },
+    // 'cricket': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'cyclommatus stag': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'damselfly': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'darner dragonfly': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'diving beetle': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'drone beetle': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'dung beetle': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    // 'earth-boring dung beetle': {
+    //   'price': ,
+    //   'location': '',
+    //   'time': [],
+    //   'months': []
+    // },
+    'emperor butterfly': {
+      'price': 4000,
+      'location': 'flying around',
+      'time': [17, 8],
+      'months': [1, 2, 3, 6, 7, 8, 9, 12]
+    },
+    'fly': {
+      'price': 60,
+      'location': 'on trash items',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'great purple emperor': {
+      'price': 3000,
+      'location': 'flying around',
+      'time': [4, 19],
+      'months': [5, 6, 7, 8]
+    },
+    'hermit crab': {
+      'price': 1000,
+      'location': 'disguised as shells on the beach',
+      'time': [19, 8],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'honeybee': {
+      'price': 200,
+      'location': 'flying around',
+      'time': [8, 17],
+      'months': [3, 4, 5, 6, 7]
+    },
+    'ladybug': {
+      'price': 200,
+      'location': 'on flowers',
+      'time': [8, 17],
+      'months': [3, 4, 5, 6, 10]
+    },
+    'man-faced stink bug': {
+      'price': 1000,
+      'location': 'on flowers',
+      'time': [19, 8],
+      'months': [3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    'mantis': {
+      'price': 430,
+      'location': 'on flowers',
+      'time': [8, 17],
+      'months': [3, 4, 5, 6, 7, 8, 9, 10, 11]
+    },
+    'mole cricket': {
+      'price': 500,
+      'location': 'underground',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 11, 12]
+    },
+    'moth': {
+      'price': 130,
+      'location': 'flying by light',
+      'time': [19, 4],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'orchid mantis': {
+      'price': 2400,
+      'location': 'on white flowers',
+      'time': [8, 17],
+      'months': [3, 4, 5, 6, 7, 8, 9, 10, 11]
+    },
+    'paper kite butterfly': {
+      'price': 1000,
+      'location': 'flying around',
+      'time': [8, 19],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'peacock butterfly': {
+      'price': 2500,
+      'location': 'flying by hybrid flowers',
+      'time': [4, 19],
+      'months': [3, 4, 5, 6]
+    },
+    'pill bug': {
+      'price': 250,
+      'location': 'by hitting rocks',
+      'time': [23, 16],
+      'months': [1, 2, 3, 4, 5, 6, 9, 10, 11, 12]
+    },
+    'snail': {
+      'price': 250,
+      'location': 'on rocks when raining',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'spider': {
+      'price': 480,
+      'location': 'by shaking trees',
+      'time': [19, 8],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'stinkbug': {
+      'price': 120,
+      'location': 'on flowers',
+      'time': [0, 23],
+      'months': [3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    'tarantula': {
+      'price': 8000,
+      'location': 'on the ground',
+      'time': [19, 4],
+      'months': [1, 2, 3, 4, 11, 12]
+    },
+    'tiger beetle': {
+      'price': 1500,
+      'location': 'on the ground',
+      'time': [0, 23],
+      'months': [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    'tiger butterfly': {
+      'price': 240,
+      'location': 'flying around',
+      'time': [4, 19],
+      'months': [3, 4, 5, 6, 7, 8, 9]
+    },
+    'wasp': {
+      'price': 2500,
+      'location': 'by shaking trees',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'wharf roach': {
+      'price': 200,
+      'location': 'on beach rocks',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'yellow butterfly': {
+      'price': 160,
+      'location': 'flying around',
+      'time': [4, 19],
+      'months': [3, 4, 5, 6, 9, 10]
+    },
 
-   // FISH LIST
+    // FISH LIST
 
-   'bitterling': {
-     'price': 900,
-     'location': 'in the river',
-     'time': [0, 23],
-     'months': [1, 2, 3, 11, 12]
-   },
-   'football fish': {
-     'price': 2500,
-     'location': 'in the sea',
-     'time': [16, 9],
-     'months': [1, 2, 3, 11, 12]
-   },
-   'sea butterfly': {
-     'price': 1000,
-     'location': 'in the sea',
-     'time': [0, 23],
-     'months': [1, 2, 3, 12]
-   },
-   'stringfish': {
-     'price': 15000,
-     'location': 'in the clifftop river',
-     'time': [16, 9],
-     'months': [1, 2, 3, 12]
-   },
-   'sturgeon': {
-     'price': 10000,
-     'location': 'in the river mouth',
-     'time': [0, 23],
-     'months': [1, 2, 3, 9, 10, 11, 12]
-   },
-   'yellow perch': {
-     'price': 300,
-     'location': 'in the river',
-     'time': [0, 23],
-     'months': [1, 2, 3, 10, 11, 12]
-   },
+    'anchovy': {
+      'price': 200,
+      'location': 'in the sea',
+      'time': [4, 21],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'barred knifejaw': {
+      'price': 5000,
+      'location': 'in the sea',
+      'time': [0, 23],
+      'months': [3, 4, 5, 6, 7, 8, 9, 10, 11]
+    },
+    'barreleye': {
+      'price': 15000,
+      'location': 'in the sea',
+      'time': [21, 4],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'bitterling': {
+      'price': 900,
+      'location': 'in the river',
+      'time': [0, 23],
+      'months': [1, 2, 3, 11, 12]
+    },
+    'black bass': {
+      'price': 400,
+      'location': 'in the river',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    'blue marlin': {
+      'price': 10000,
+      'location': 'at the pier',
+      'time': [0, 23],
+      'months': [1, 2, 3, 4, 7, 8, 9, 11, 12]
+    },
+    'football fish': {
+      'price': 2500,
+      'location': 'in the sea',
+      'time': [16, 9],
+      'months': [1, 2, 3, 11, 12]
+    },
+    'sea butterfly': {
+      'price': 1000,
+      'location': 'in the sea',
+      'time': [0, 23],
+      'months': [1, 2, 3, 12]
+    },
+    'stringfish': {
+      'price': 15000,
+      'location': 'in the clifftop river',
+      'time': [16, 9],
+      'months': [1, 2, 3, 12]
+    },
+    'sturgeon': {
+      'price': 10000,
+      'location': 'in the river mouth',
+      'time': [0, 23],
+      'months': [1, 2, 3, 9, 10, 11, 12]
+    },
+    'yellow perch': {
+      'price': 300,
+      'location': 'in the river',
+      'time': [0, 23],
+      'months': [1, 2, 3, 10, 11, 12]
+    },
   }
 }
